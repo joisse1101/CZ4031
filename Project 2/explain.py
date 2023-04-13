47cfb8bed8b72990caeab61ee8105291ee657494
jwenT96 <jjww96@gmail.com>
2023-04-13 10:45:27 +0800

    Removed main and minor changes

--- Project 2/explain.py
+++ Project 2/explain.py
@@ -8,19 +8,13 @@ RECT_HEIGHT = 60
 CANVAS_WIDTH = 1000
 CANVAS_HEIGHT = 1000
 
-all_nodes=[]
-visual_to_node={}
-
-
+all_nodes = []
+visual_to_node = {}
 
 
 # Class that converts query plan into natural language -- ENGLISH
 class Plan():
-    def __init__(self , x1,x2,y1,y2):
-        self.x1=x1
-        self.x2=x2
-        self.y1=y1
-        self.x2=y2        
+    def __init__(self):
         self.root = None
         self.q1_info = None
         self.q2_info = None
@@ -33,7 +27,7 @@ class Plan():
         if 'Plans' in plan:
             for childPlan in plan["Plans"]:
 
-                childNode = Node()
+                childNode = Node(1, 1, 1, 1)
                 childNode.setupNode(plan=childPlan, child=False)
                 self.operations.append(childNode.nodeType)
                 self.information.append(childNode.annotation)
@@ -41,7 +35,7 @@ class Plan():
                 self.traversePlans(childNode, childPlan)
 
     def initialisePlans(self, plan):
-        self.root = Node()  # Initialise
+        self.root = Node(1, 1, 1, 1)  # Initialise
         self.root.setupNode(plan=plan, child=False)
         self.totalCost = self.root.cost
         self.operations.append(self.root.nodeType)
@@ -59,7 +53,12 @@ class Plan():
 
 class Node():
 
-    def __init__(self):
+    def __init__(self, x1, x2, y1, y2):
+
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
 
         self.nodeType = None
         self.strategy = None
@@ -148,54 +147,49 @@ class Node():
 
         else:
             self.annotation = f"<b>{self.nodeType}</b> operation was performed."
-    
-    
-    def draw(query_plan , canvas):
+
+    def draw(query_plan, canvas):
         data = json.loads(query_plan)
         all_nodes.clear()
-        
-        root_op = Plan(500,500+RECT_WIDTH, 0 , 0+RECT_HEIGHT,'','')
-        build_plan =(root_op,data)
-        
-        # creation of the rectangle 
-        
+
+        root_op = Plan(500, 500+RECT_WIDTH, 0, 0+RECT_HEIGHT, '', '')
+        build_plan = (root_op, data)
+
+        # creation of the rectangle
+
         uniq_coord = []
         for item in all_nodes:
-            coord = (item.x1, item.x2 , item.y1 , item.y2 )
+            coord = (item.x1, item.x2, item.y1, item.y2)
             if coord not in uniq_coord:
                 uniq_coord.append(coord)
             else:
-                item.x1+= 3*RECT_WIDTH/2
-                item.x2+= 3*RECT_WIDTH/2
-                item.center = (((item.x1 + item.x2)/2),((item.y1+item.y2)/2))
-                new_coord = (item.x1, item.x2 , item.y1 , item.y2 )
+                item.x1 += 3*RECT_WIDTH/2
+                item.x2 += 3*RECT_WIDTH/2
+                item.center = (((item.x1 + item.x2)/2), ((item.y1+item.y2)/2))
+                new_coord = (item.x1, item.x2, item.y1, item.y2)
                 uniq_coord.append(new_coord)
-        
+
         for item in all_nodes:
             x1 = item.x1
             x2 = item.x2
             y1 = item.y1
             y2 = item.y2
-            rect = canvas.create_rectangle(x1,y1,x2,y2)
-            
+            rect = canvas.create_rectangle(x1, y1, x2, y2)
+
             balloon = Pmw.Balloon()
-            balloon.tagbind(canvas, rect,item.information)
+            balloon.tagbind(canvas, rect, item.information)
             visual_to_node[rect] = item
-            
-            
+
         for item in all_nodes:
-            gui_text = canvas.create_text((item.center[0], item.center[1]),text = item.operation)
-            visual_to_node[gui_text] = item 
-        
+            gui_text = canvas.create_text(
+                (item.center[0], item.center[1]), text=item.operation)
+            visual_to_node[gui_text] = item
+
         for item in all_nodes:
             for child in item.children:
-                            canvas.create_line(child.center[0], child.center[1] - RECT_HEIGHT/2, item.center[0],
-                               item.center[1] + RECT_HEIGHT/2, arrow=tk.LAST)
-                
-        
-    
-    
+                canvas.create_line(child.center[0], child.center[1] - RECT_HEIGHT/2, item.center[0],
+                                   item.center[1] + RECT_HEIGHT/2, arrow=tk.LAST)
+
     if __name__ == '__main__':
 
         draw("query_plan.json")
-        
--- Project 2/project.py
+++ Project 2/project.py
@@ -3,6 +3,9 @@ import psycopg2
 from connection import *
 from explain import *
 
+import interface
+
+
 # Process the csv files to remove extra"|"
 
 
@@ -21,7 +24,7 @@ def processCSVFile():
                 w.write('\n')
 
 
-def displayPlan(plan):
+def displayPlan(query_plan):
     plan = Plan()
     plan.initialisePlans(query_plan)  # From explain.py
     fullAnnotation = plan.getPlanAnnotations()
@@ -31,7 +34,20 @@ def displayPlan(plan):
     return plan
 
 
-if __name__ == '__main__':
+def testDummyQuery():
+
+    connection = setupConnection(
+        "localhost", 5432, "TPC-H", "postgres", "922858")
+    query_1 = "select l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price, sum(l_extendedprice * (1 - l_discount)) as sum_disc_price, sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order from lineitem where l_extendedprice > 100 group by l_returnflag, l_linestatus order by l_returnflag, l_linestatus;"
+    query_2 = "select ps_partkey, sum(ps_supplycost * ps_availqty) as value from partsupp, supplier, nation where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'GERMANY' and ps_supplycost > 20 and s_acctbal > 10 group by ps_partkey having sum(ps_supplycost * ps_availqty) > ( select sum(ps_supplycost * ps_availqty) * 0.0001000000 from partsupp, supplier, nation where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'GERMANY' ) order by value desc;"
+    query_3 = "select o_orderpriority, count(*) as order_count from orders where o_totalprice > 100 and exists ( select * from lineitem where l_orderkey = o_orderkey and l_extendedprice > 100 ) group by o_orderpriority order by o_orderpriority;"
+
+    print("For FIRST Query: \n")
+    query_plan = connection.generatePlan(query_1)
+    firstPlan = displayPlan(query_plan)
+
+
+def setupConnection(host, port, database, username, password):
     # Metadata to initialise connection with the POSTGRESQL
     # Port Number 5432 by Default
     conn = None
@@ -40,30 +56,22 @@ if __name__ == '__main__':
     # Create variables for connection
 
     conn = psycopg2.connect(
-        host="localhost",
-        database="TPC-H",
-        user="postgres",
-        password="922858")
+        host=host,
+        port=port,
+        database=database,
+        user=username,
+        password=password)
 
     connection = Connection(conn)
     print("SUCCESS: Connected established.")
+    return connection
 
-    # Will be good if user can get sample/ dummy query
-    # QUERY 1
-    query_1 = "select l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price, sum(l_extendedprice * (1 - l_discount)) as sum_disc_price, sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order from lineitem where l_extendedprice > 100 group by l_returnflag, l_linestatus order by l_returnflag, l_linestatus;"
-    # QUERY 2
-    query_2 = "select ps_partkey, sum(ps_supplycost * ps_availqty) as value from partsupp, supplier, nation where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'GERMANY' and ps_supplycost > 20 and s_acctbal > 10 group by ps_partkey having sum(ps_supplycost * ps_availqty) > ( select sum(ps_supplycost * ps_availqty) * 0.0001000000 from partsupp, supplier, nation where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'GERMANY' ) order by value desc;"
-    query_3 = "select o_orderpriority, count(*) as order_count from orders where o_totalprice > 100 and exists ( select * from lineitem where l_orderkey = o_orderkey and l_extendedprice > 100 ) group by o_orderpriority order by o_orderpriority;"
 
-    query_1 = "select * from customer C, orders O where C.c_custkey = O.o_custkey;"
-    query_2 = f"select * from customer C, orders O where C.c_custkey = O.o_custkey and C.c_name like '%23';"
+if __name__ == '__main__':
 
-    # First Query =======================================
-    print("For FIRST Query: \n")
-    query_plan = connection.generatePlan(query_1)
-    firstPlan = displayPlan(query_plan)
-
-    # First Query =======================================
-    print("For SECOND Query: \n")
-    query_plan = connection.generatePlan(query_2)
-    secondPlan = displayPlan(query_plan)
+    connectMetadata = interface.GUI().initialise_GUI()
+    if connectMetadata is not None:
+        host, port, database, username, password = connectMetadata
+        SQL_connect = setupConnection(host, port, database, username, password)
+        interface.GUI().main_window(SQL_connect)
+    # testDummyQuery()
