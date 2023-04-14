345511f4b7cf073716cfc6c0a704e57d79668ad5
Vignesh Ezhil <86967110+viggy2000@users.noreply.github.com>
2023-04-14 19:05:21 +0800

    saving progress

--- Project 2/interface.py
+++ Project 2/interface.py
@@ -9,64 +9,64 @@ class GUI:
     def __init__(self):
         return
     
-    # def initialise_GUI(self):
+    def initialise_GUI(self):
         
-    #     font = 'Consolas'
+        font = 'Consolas'
         
-    #     col1 = [
-    #         [sg.Text('Host:', font=(font, 12), justification='left')],
-    #         [sg.Text('Port:', font=(font, 12), justification='left')],
-    #         [sg.Text('Database:', font=(font, 12), justification='left')],
-    #         [sg.Text('Username:', font=(font, 12), justification='left')],
-    #         [sg.Text('Password:', font=(font, 12), justification='left')],
-    #     ]
+        col1 = [
+            [sg.Text('Host:', font=(font, 12), justification='left')],
+            [sg.Text('Port:', font=(font, 12), justification='left')],
+            [sg.Text('Database:', font=(font, 12), justification='left')],
+            [sg.Text('Username:', font=(font, 12), justification='left')],
+            [sg.Text('Password:', font=(font, 12), justification='left')],
+        ]
 
-    #     col2 = [
-    #         [sg.Input(key='host', font=(font, 12), default_text='localhost')],
-    #         [sg.Input(key='port', font=(font, 12), default_text='5432')],
-    #         [sg.Input(key='database', font=(font, 12), default_text='TPC-H')],
-    #         [sg.Input(key='username', font=(font, 12), default_text='postgres')],
-    #         [sg.InputText('', key='password', password_char='*', font=(font, 12))],
-    #     ]
+        col2 = [
+            [sg.Input(key='host', font=(font, 12), default_text='localhost')],
+            [sg.Input(key='port', font=(font, 12), default_text='5432')],
+            [sg.Input(key='database', font=(font, 12), default_text='TPC-H')],
+            [sg.Input(key='username', font=(font, 12), default_text='postgres')],
+            [sg.InputText('', key='password', password_char='*', font=(font, 12))],
+        ]
         
-    #     layout = [  
-    #         [sg.Text('Query Execution Plan Annotator', key='-text-', font=(font, 20))],
-    #         [sg.Frame(layout=col1, title=''), sg.Frame(layout=col2, title='')],
-    #         [sg.Button('Submit')]
-    #     ]
+        layout = [  
+            [sg.Text('Query Execution Plan Annotator', key='-text-', font=(font, 20))],
+            [sg.Frame(layout=col1, title=''), sg.Frame(layout=col2, title='')],
+            [sg.Button('Submit')]
+        ]
         
         
-    #     # Create the Window
-    #     window = sg.Window('CX4031 Project 2 GUI', layout, element_justification='c', return_keyboard_events=True).Finalize()
-    #     window.bind("<Return>", "_Enter")
+        # Create the Window
+        window = sg.Window('CX4031 Project 2 GUI', layout, element_justification='c', return_keyboard_events=True).Finalize()
+        window.bind("<Return>", "_Enter")
         
-    #     while True:
-    #         event, values = window.read()
-    #         if event == 'Submit':   # when user clicks submit button
+        while True:
+            event, values = window.read()
+            if event == 'Submit':   # when user clicks submit button
 
-    #             # get all inputs
-    #             host = values['host'].lower()   #localhost
-    #             port = values['port']   #5432
-    #             database = values['database']   #whatever ur database name is
-    #             username = values['username'].lower()   #postgres
-    #             password = values['password']   #whatever ur password is
-    #             window.close()
-    #             return host, port, database, username, password
+                # get all inputs
+                host = values['host'].lower()   #localhost
+                port = values['port']   #5432
+                database = values['database']   #whatever ur database name is
+                username = values['username'].lower()   #postgres
+                password = values['password']   #whatever ur password is
+                window.close()
+                return host, port, database, username, password
 
-    #         elif event == "_Enter":
-    #             # get all inputs
-    #             host = values['host'].lower()   #localhost
-    #             port = values['port']   #5432
-    #             database = values['database']   #whatever ur database name is
-    #             username = values['username'].lower()   #postgres
-    #             password = values['password']   #whatever ur password is
-    #             window.close()
-    #             return host, port, database, username, password
+            elif event == "_Enter":
+                # get all inputs
+                host = values['host'].lower()   #localhost
+                port = values['port']   #5432
+                database = values['database']   #whatever ur database name is
+                username = values['username'].lower()   #postgres
+                password = values['password']   #whatever ur password is
+                window.close()
+                return host, port, database, username, password
 
-    #         if event == sg.WIN_CLOSED: # if user closes window
-    #             break
+            if event == sg.WIN_CLOSED: # if user closes window
+                break
 
-    #     window.close()
+        window.close()
         
     def main_window(self,connect):
         
