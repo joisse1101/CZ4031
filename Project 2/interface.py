4e53eae552867dc04cddb54860578da83a46af31
Vignesh Ezhil <86967110+viggy2000@users.noreply.github.com>
2023-04-14 19:16:48 +0800

    updated files

--- Project 2/interface.py
+++ Project 2/interface.py
@@ -75,6 +75,7 @@ class GUI:
         root = tk.Tk()
         root.title("Query Visualiser")
         root.state('zoomed')
+
         def close():
             root.destroy()
             return 
@@ -95,6 +96,71 @@ class GUI:
         # root.bind_all("<Control-q>" , lambda e : close())
         # root.mainloop()
         
+    def new_query(self):
+        font = 'Consolas'
+
+        row = [[[sg.Text('Enter Query:', font=(font, 12), justification='left')],[sg.Text('Enter Query 2:', font=(font, 12), justification='left')], [sg.Multiline(
+                            size=(45, 15),
+                            key="query", 
+                            font=(font, 12), 
+                            autoscroll=True
+                        )]]
+        ]
+
+        # row1 = [
+        #     [sg.Text('Query 1 :', font=(font, 12), justification='left')],
+        #     [sg.Text('Query 2: ', font=(font, 12), justification='left')],
+        # ]
+
+        # Create 3 rows of toggles with 4 toggles in each row
+        col1 = [[sg.Checkbox('Bitmap scan', key='bitmap_scan', default=True)],
+                        [sg.Checkbox('Hashagg', key='hashagg', default=True)],
+                        [sg.Checkbox('Hashjoin', key='hashjoin', default=True)],
+                        [sg.Checkbox('Index scan', key='index_scan', default=True)]
+                        ]
+        
+        col2 = [[sg.Checkbox('Index Only scan', key='index_only_scan', default=True)],
+                        [sg.Checkbox('Material', key='material', default=True)],
+                        [sg.Checkbox('Merge Join', key='merge_join', default=True)],
+                        [sg.Checkbox('Nested Loop', key='nested_loop', default=True)]
+        ]
+
+        col3 = [[sg.Checkbox('Seq Scan', key='seq_scan', default=True)],
+                        [sg.Checkbox('Sort', key='sort', default=True)],
+                        [sg.Checkbox('Tidscan', key='tidscan', default=True)],
+                        [sg.Checkbox('Gather merge', key='gather_merge', default=True)]
+                        ]
+        
+        layout = [  
+            row,
+            [sg.Frame(layout=col1, title=''), sg.Frame(layout=col2, title=''), sg.Frame(layout=col3, title='')],
+            [sg.Button('Submit')]
+        ]
+
+        window = sg.Window('CX4031 Project 2 GUI', layout, element_justification='c').Finalize()
+
+        while True:
+            event, values = window.read()
+            if event == 'Submit':   # when user clicks submit button
+                if(values['query'] != ''):
+                    self.query = values['query'] #get query
+                    self.apqs = [values['bitmap_scan'], values['hashagg'], values['hashjoin'], values['index_scan'], values['index_only_scan'], values['material'], values['merge_join'], values['nested_loop'], values['seq_scan'], values['sort'], values['tidscan'], values['gather_merge']]
+                    break
+
+            if event == sg.WIN_CLOSED: # if user closes window
+                break
+
+        window.close()
+
+        if self.query != '':
+            self.json_query = self.connect.getQueryPlan(self.query, params=self.apqs)
+            self.query_json_canvas.itemconfig("jsonquery", text = self.json_query)
+            self.structure_query()
+            self.query_text_canvas.itemconfig("querytext", text = self.query)
+            self.canvas.delete('all')
+            draw(self.json_query, self.canvas)
+
+
         
         
         
